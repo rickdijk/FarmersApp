8e78d246580907996adf751249935aefcef50325
Rick van Dijk <rickavdijk@gmail.com>
2018-05-09 16:08:50 +0200

    Version 1.1, added at Heemservaart

--- source/FarmersApp/FarmersApp/Farmer.cs
+++ source/FarmersApp/FarmersApp/Farmer.cs
@@ -8,5 +8,29 @@ namespace FarmersApp
 {
     class Farmer
     {
+        public int BagsOfFeed { get; set; }
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            // (add the get and set accessors from the
+            // previous page)
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
     }
 }
